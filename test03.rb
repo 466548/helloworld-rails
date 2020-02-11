c736b928bfb5ec13223cff868fb199a2ad6ec1d8
466548 <tsuka.as@gmail.com>
2020-02-12 00:26:05 +0900

    変数で呼び出しする

---
+++ test03.rb
@@ -0,0 +1,13 @@
+# -*- config: utf-8 -*-
+# strlという変数名でこの文字列を参照できるようになる
+strl = "ここが文字列"
+
+# strl２という変数名でこの文字列も参照できるお
+strl2 = 'こここも文字列'
+
+#strl3は1と同じ文字列
+strl3 = strl
+
+puts strl;
+puts strl2;
+puts strl3;
